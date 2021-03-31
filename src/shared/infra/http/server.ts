6747d2ef3815f4c20acab94269eeef73e0ebce8e
PabloMelo <pablomelodev@gmail.com>
2021-03-31 20:15:18 -0300

    chore: create seed user admin

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,11 +4,13 @@ import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
 import '@shared/container';
-import '@shared/infra/typeorm';
+import createConnection from '@shared/infra/typeorm';
 import { AppError } from '@shared/errors/AppError';
 
 import swaggerFile from '../../../swagger.json';
 import { router } from './routes';
+
+createConnection();
 
 const app = express();
 app.use(express.json());
