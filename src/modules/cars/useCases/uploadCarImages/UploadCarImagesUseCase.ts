3f7d0939441e0599f392dd4d00fec1fa93e8dafa
PabloMelo <pablomelodev@gmail.com>
2021-04-13 13:15:58 -0300

    feat: upload image car

--- src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
@@ -1,6 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 
 import { ICarImagesRepository } from '@modules/cars/repositories/ICarImagesRepository';
+import { IStorageProvider } from '@shared/container/providers/StorageProvider/IStorageProvider';
 
 import { IRequestUploadCarImagesDTO } from '@modules/cars/dtos/IRequestUploadCarImagesDTO';
 
@@ -9,6 +10,9 @@ class UploadCarImagesUseCase {
   constructor(
     @inject('CarImagesRepository')
     private carImagesRepository: ICarImagesRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) {}
 
   async execute(data: IRequestUploadCarImagesDTO): Promise<void> {
@@ -16,6 +20,11 @@ class UploadCarImagesUseCase {
       await this.carImagesRepository.create({
         car_id: data.car_id,
         image_name: image,
+      });
+
+      await this.storageProvider.save({
+        file: image,
+        folder: 'cars',
       });
     });
   }
